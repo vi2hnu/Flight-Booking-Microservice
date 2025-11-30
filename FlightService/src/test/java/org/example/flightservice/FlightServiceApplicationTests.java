5b2a8656ddc444c4b4cfca30a049dcf887e5dbef
vi2hnu <vishnuvaradhankr@gmail.com>
2025-11-30 08:35:05 +0530

    initial commit for flight service

---
+++ FlightService/src/test/java/org/example/flightservice/FlightServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.flightservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
